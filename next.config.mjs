38a9d7c6c94ff818203d50276847841ae739aec0
Alejandro Jaramillo <alejojaramillo96@gmail.com>
2025-04-05 21:55:19 +0200

    Run rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -34,7 +34,7 @@ const nextConfig = {
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/:path*"
-            : "/api/",
+            : "/api/:path*",
       },
       {
         source: "/docs",
